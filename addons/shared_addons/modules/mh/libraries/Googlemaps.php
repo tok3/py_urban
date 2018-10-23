36ae9a02d020ed1ae3fbe9f20627a2f5e8847771
Tobias Koch <tok3@eq3w.de>
2018-10-23 13:17:22 +0200

    google api key added

--- addons/shared_addons/modules/mh/libraries/Googlemaps.php
+++ addons/shared_addons/modules/mh/libraries/Googlemaps.php
@@ -2699,7 +2699,11 @@ class Googlemaps
 
         }
 
+        echo "<pre>";
+        print_r($url);
+        echo "</pre>";
 
+        die();
 
         $json_result = json_decode($this->file_get_contents_curl($url));
 
